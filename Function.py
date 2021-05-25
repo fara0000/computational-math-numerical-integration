8dbadcce721e565e90a5fff39ae9d448e1f0f86c
fara0000 <fexri_imanzade@mail.ru>
2021-05-25 14:16:28 +0300

    logic of integration with all three numerical methods done

---
+++ Function.py
@@ -0,0 +1,12 @@
+import math
+from dataclasses import dataclass
+from typing import Callable
+
+
+@dataclass
+class Function:
+    function: Callable
+    text: str
+    symmetrical: bool = False
+    symmetry_point: float = math.inf
+    antiderivative: Callable = None
